c447a71b3c0261591f9780c48d4c69e6babed73f
himanshu <himanshu73188@gmail.com>
2021-02-28 23:16:45 +0530

    added the code to display persons

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../shared/ui.service';
-import { PersonService } from './shared/person.service';
+import { Person, PersonService } from './shared/person.service';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-persons',
@@ -10,31 +11,35 @@ import { PersonService } from './shared/person.service';
 export class PersonsComponent implements OnInit {
 
   // booleans
-  isLoading:boolean                    = true;
+  isLoading:boolean         = true;
+
+  // other variables
+  personsData:Person | any  = [];
 
   constructor(
     private ui              : UiService,
     private persons         : PersonService,
-  ) { }
+  ) { 
+    AOS.init();
+  }
 
   ngOnInit(): void {
     this.fetchPersons();
   }
 
   fetchPersons() {
-
+    // fetch all persons
     this.persons.fetchPersons()
-    .subscribe(data => {
-        if(data.status == 'success') {
-          console.log(data);
-          this.isLoading                = false;
+    .subscribe(result => {
+        if(result.status == 'success') {
+          this.personsData  = result.data.map(item => ({...item, showText: false})); // add a new key 'showText' to each item in the array
+          this.isLoading    = false;
         } else {
           this.ui.showSnackbar("Something is wrong with the API call. Please try again. If you continue to have issue please notify the tech team.", "failure");
         }
     }, err => {
       this.ui.showSnackbar("Something is wrong with the API call. Please try again. If you continue to have issue please notify the tech team.", "failure");
     });
-
   }
 
 }
--- src/app/persons/shared/person.service.ts
+++ src/app/persons/shared/person.service.ts
@@ -2,6 +2,24 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 import { environment } from "src/environments/environment";
 
+// person interface
+export interface Person {
+  id            : number;
+  biography     : string;
+  date_of_birth : string;
+  name          : string;
+  metadata      : {
+    tags        :Array<string>
+  },
+  showText      : boolean
+}
+
+// api result interface
+interface fetchPersonsResult {
+  status        : string,
+  data          : Person[]
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,9 +28,11 @@ export class PersonService {
   constructor(
     private http      : HttpClient,
   ) { }
-
+  
+  // fetch all persons 
+  // /v1/persons
   fetchPersons() {
-    return this.http.get<any>(environment.apiUrl + '/v1/persons');
+    return this.http.get<fetchPersonsResult>(environment.apiUrl + '/v1/persons');
   } 
 
 }
